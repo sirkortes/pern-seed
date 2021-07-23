37ee3ff4ce434836be16e11d80b0035717d8230f
sirkriz <kriz@krizcortes.com>
2021-07-23 12:21:37 -0400

    setup create todo route

--- server/index.js
+++ server/index.js
@@ -12,7 +12,16 @@ app.use(express.json())
 
 // routes
 
-// create todo
+// create todo with async so it waits for db process
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body
+    const newTodo = await pool.query('INSERT INTO todo (description) VALUES($1) RETURNING *', [description])
+    res.json(newTodo.rows[0])
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 // get all todos
 // get a todo
 // update a todo
