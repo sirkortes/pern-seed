80992c2984158f8cf563f6401e65fe321a18891f
sirkriz <kriz@krizcortes.com>
2021-07-23 13:50:25 -0400

    build rest of crud api routes

--- server/index.js
+++ server/index.js
@@ -11,8 +11,17 @@ app.use(cors())
 app.use(express.json())
 
 // routes
+/*
+app.post('/todos', async (req, res) => {
+  try {
+    
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+*/
 
-// create todo with async so it waits for db process
+// create a todo with async so it waits for db process
 app.post('/todos', async (req, res) => {
   try {
     const { description } = req.body
@@ -22,10 +31,50 @@ app.post('/todos', async (req, res) => {
     console.error(err.message)
   }
 })
+
 // get all todos
+app.get('/todos', async (req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo')
+    res.json(allTodos.rows)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
 // get a todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [id])
+    res.json(todo.rows[0])
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
 // update a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const { description } = req.body
+    const updateTodo = await pool.query('UPDATE todo SET description = $1 WHERE todo_id = $2', [description, id])
+    res.json('Todo was updated!')
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
 // delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleteTodo = await pool.query('DELETE FROM todo WHERE todo_id = $1', [id])
+    res.json('Todo was deleted!')
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`server has started on port: ${PORT}`)
