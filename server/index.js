f84416f24ed108798f55a74429d2ecaa44863566
sirkriz <kriz@krizcortes.com>
2021-07-23 11:58:56 -0400

    create and connect postgres db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const pool = require('./db')
 
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 // middleware
 app.use(cors())
 app.use(express.json())
 
+// routes
+
+// create todo
+// get all todos
+// get a todo
+// update a todo
+// delete a todo
+
 app.listen(PORT, () => {
-  console.log(`server has started on port ${PORT}`)
+  console.log(`server has started on port: ${PORT}`)
+  console.log(`connected to db: ${pool.options.host}[${pool.options.port}]${pool.options.database}`)
 })
